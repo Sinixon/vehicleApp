a726c14d0d712191aee95ae1572b8dcc94eb7e8e
Sinixon <joeyburgt@hotmail.com>
2023-08-25 22:10:37 +0200

    Add delete & post requests

--- demo/src/main/java/com/example/vehicleApp/vehicle/VehicleController.java
+++ demo/src/main/java/com/example/vehicleApp/vehicle/VehicleController.java
@@ -20,6 +20,19 @@ public class VehicleController {
     }
     @PostMapping
     public void registerNewVehicle(@RequestBody Vehicle vehicle) {
+        // TODO: Build a check that available <= total & total > 0 & available >= 0
         vehicleService.addNewVehicle(vehicle);
+    }
+    @DeleteMapping(path = "{vehicleId}")
+    public void deleteVehicle(@PathVariable("vehicleId") Long vehicleId) {
+        vehicleService.deleteVehicle(vehicleId);
+    }
+
+    @PutMapping(path = "{vehicleId}")
+    public void updateVehicle(@PathVariable("vehicleId") Long vehicleId,
+                              @RequestParam(required = false) String name,
+                              @RequestParam(required = false) Integer available,
+                              @RequestParam(required = false) Integer total) {
+        vehicleService.updateStudent(vehicleId, name, available, total);
     }
 }
--- demo/src/main/java/com/example/vehicleApp/vehicle/VehicleService.java
+++ demo/src/main/java/com/example/vehicleApp/vehicle/VehicleService.java
@@ -2,15 +2,18 @@ package com.example.vehicleApp.vehicle;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
 public class VehicleService {
 
     private final VehicleRepository vehicleRepository;
-@Autowired
+    @Autowired
     public VehicleService(VehicleRepository vehicleRepository) {
         this.vehicleRepository = vehicleRepository;
     }
@@ -25,5 +28,36 @@ public class VehicleService {
         }
 
         vehicleRepository.save(vehicle);
+    }
+
+    public void deleteVehicle(Long vehicleId) {
+        boolean exists = vehicleRepository.existsById(vehicleId);
+
+        if (!exists) {
+            //Make better message? Id says nothing.
+            throw new IllegalStateException("Vehicle with id " + vehicleId + " does not exists");
+        }
+        vehicleRepository.deleteById(vehicleId);
+    }
+
+    @Transactional
+    public void updateStudent(Long vehicleId, String name, int available, int total) {
+        Vehicle vehicle = vehicleRepository.findById(vehicleId)
+                .orElseThrow(() -> new IllegalStateException("Vehicle with name " + name + " does not exists."));
+
+        if (name != null && name.length() > 0 && !Objects.equals(vehicle.getName(), name)) {
+            vehicle.setName(name);
+        }
+
+        // Check if total amount of the vehicle being updated is atleast 1 and available is atleast 0 or more
+        if (available >= 0 && total > 0) {
+            if (available > total) {
+                throw new IllegalStateException("The amount of vehicles available cannot exceed the total amount of vehicles present");
+            }
+            vehicle.setAvailable(available);
+            vehicle.setTotal(total);
+        }
+
+        //TODO: Make a request without available and total
     }
 }
